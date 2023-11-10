a6938123651934b05d7df4c5aad0f6f751cb647c
Mazharul Hoque <mazharul15-14157@diu.edu.bd>
2023-11-10 21:22:44 +0600

    add user home

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
-        return view('home');
+        $blogs = Blog::with('category')->get();
+        return view('home', compact('blogs'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-    
-
-    <div class="mb-5">
-    <div class="card mb-3" >
-        <div class="row g-0">
-            <div class="col-md-6 pe-4">
-                <img src="{{asset('phptp/photo1.avif')}}" class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-6">
-                <div class="card-body ">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                    <p class="card-text" style="margin: 0;"><small class="category">Category</small></p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <div>
+        @foreach ($blogs as $item)
+            @if ($loop->iteration % 2 == 1)
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-6 pe-4">
+                            <img src="{{ asset('phptp/photo1.avif') }}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-6 d-flex flex-grow-1 justify-content-center align-items-center pe-3">
+                            <div class="card-body ">
+                                <h5 class="card-title">{{ $item->title }}</h5>
+                                <p class="card-text">{{ $item->description }}</p>
+                                <p class="card-text" style="margin: 0;"><small class="category">Category <small
+                                            class="text-muted">in</small> {{ $item->category->name }}</small></p>
+                                <p class="card-text"><small class="text-muted">Last updated at
+                                        {{ Str::limit($item->updated_at, 10, '') }}</small></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            @else
+                <div class="card mb-3">
+                    <div class="row g-0">
+
+                        <div class="col-md-6 d-flex flex-grow-1 justify-content-center align-items-center ps-3">
+                            <div class="card-body ">
+                                <h5 class="card-title">{{ $item->title }}</h5>
+                                <p class="card-text">{{ $item->description }}</p>
+                                <p class="card-text" style="margin: 0;"><small class="category">Category <small
+                                            class="text-muted">in</small> {{ $item->category->name }}</small></p>
+                                <p class="card-text"><small class="text-muted">Last updated at
+                                        {{ Str::limit($item->updated_at, 10, '') }}</small></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 ps-4">
+                            <img src="{{ asset('phptp/photo1.avif') }}" class="img-fluid rounded-end" alt="...">
+                        </div>
+                    </div>
+                </div>
+            @endif
+        @endforeach
     </div>
-
-    <div class="card mb-3" >
-        <div class="row g-0">
-            <div class="col-md-6">
-                <img src="..." class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-6">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-        </div>
-    </div></div>
 @endsection
